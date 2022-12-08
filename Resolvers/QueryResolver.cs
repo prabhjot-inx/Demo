52903c1ca013f65a4265b98c9c7f6a32fc9b0811
Prabhjot Singh <prabhjot@insonix.com>
2022-12-08 11:02:27 +0530

    add bearer token authentication

--- Resolvers/QueryResolver.cs
+++ Resolvers/QueryResolver.cs
@@ -1,11 +1,13 @@
 using Demo.Logics;
 using Demo.InputTypes;
 using HotChocolate;
+using HotChocolate.AspNetCore.Authorization;
 
 namespace Demo.Resolvers
 {
   public class QueryResolver
   {
+    [Authorize]
     public string Welcome()
     {
       return "Welcome to the world of magic";
--- Startup.cs
+++ Startup.cs
@@ -14,6 +14,9 @@ using Demo.Data.Entities;
 using Demo.Logics;
 using Demo.Resolvers;
 using Demo.Shared;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Demo
 {
@@ -21,20 +24,54 @@ namespace Demo
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
+
+        private readonly IWebHostEnvironment _env;
 
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AuthContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.Configure<TokenSettings>(Configuration.GetSection("TokenSettings"));
-            services.AddScoped<IAuthLogic, AuthLogic>();
+            
+
             services.AddGraphQLServer()
             .AddQueryType<QueryResolver>()
-            .AddMutationType<MutationResolver>();
+            .AddMutationType<MutationResolver>()
+            .AddAuthorization()
+            .ModifyRequestOptions(opt => opt.IncludeExceptionDetails = _env.IsDevelopment()); // for logging error
+            
+            services.AddDbContext<AuthContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IAuthLogic, AuthLogic>();
+            services.Configure<TokenSettings>(Configuration.GetSection("TokenSettings"));
+            
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => 
+            {
+                var tokenSettings = Configuration.GetSection("TokenSettings").Get<TokenSettings>();
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = tokenSettings.Issuer,
+                    ValidateIssuer = true,
+                    ValidAudience = tokenSettings.Audience,
+                    ValidateAudience = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Key)),
+                    ValidateIssuerSigningKey = true
+                };
+            });
+            services.AddAuthorization();
+            // services.AddAuthorization(options => {
+            //     options.AddPolicy("roles-policy", policy => {
+            //         policy.RequireRole(new string[]{"admin","super-admin"});
+            //     });
+            //     options.AddPolicy("claim-policy-1", policy => {
+            //         policy.RequireClaim("LastName");
+            //     });
+            //     options.AddPolicy("claim-policy-2", policy=>{
+            //         policy.RequireClaim("LastName",new string[]{"Bommidi","Test"});
+            //     });
+            // });
 
         }
 
@@ -45,8 +82,9 @@ namespace Demo
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGraphQL();
